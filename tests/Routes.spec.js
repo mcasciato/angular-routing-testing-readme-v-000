8e1628a004ee88e0ba81647de0669b0a71281409
mcasciato <michael.casciato@icloud.com>
2016-10-25 12:47:27 -0400

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -15,5 +15,37 @@ describe('Routes', function () {
 
 			expect(state.url).toEqual('/settings');
 		});
+
+		it('should have the correct template', function () {
+			expect(state.templateUrl).toEqual('views/settings.html');
+		});
+	});
+
+	describe('User', function () {
+		var state;
+
+		it('should have the correct URL', function () {
+			state = $state.get('settings.user');
+
+			expect(state.url).toEqual('/user');
+		});
+
+		it('should have the correct template', function () {
+			expect(state.templateUrl).toEqual('views/settings/user.html');
+		});
+	});
+
+	describe('Notifications', function () {
+		var state;
+
+		it('should have the correct URL', function () {
+			state = $state.get('settings.notifications');
+
+			expect(state.url).toEqual('/notifications');
+		});
+
+		it('should have the correct template', function () {
+			expect(state.templateUrl).toEqual('views/settings/notifications.html');
+		});
 	});
 });
